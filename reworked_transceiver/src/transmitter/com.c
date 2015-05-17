dc06d670748dec9723a8c13825cf0b8d5ef1f698
neale <nealeratzlaff@gmail.com>
2015-05-17 00:21:24 -0700

    added comments

--- reworked_transceiver/src/transmitter/com.c
+++ reworked_transceiver/src/transmitter/com.c
@@ -26,6 +26,9 @@ void initUART(void)
   UCSR1C = (1<<UCSZ11) | (1<<UCSZ10);  //1 stop bit
 }
 
+/* FIX ME */
+
+//change to look like the collab packet
 void build_packet(uint8_t * data){
 
   for(int i = 0; i < PACKET_SIZE; ++i){
@@ -48,22 +51,39 @@ void uart_putchar(char c){
   while (!(UCSR1A & (1 << UDRE1)));
   UDR1 = c;
 }
-
+/* this is a don question. I have no clue what's happening
+ * it works. when the switch is on it sends, off it doesn't
+ * and you can flip flop between modes
+ * But when you turn off the switch there is about a 10 second
+ * delay for it to actually stop transmitting
+ * I don't understand why, the program completely ignores my conditions
+ * for entering transmission for 10 seconds. 
+ *
+ * There is some UART / embedded thing going on that I don't 
+ * understand
+ */
 void transmit(uint8_t * data){
 
   SPI_masterInit();
-  initRadioTX();
-  _delay_ms(8);
-  uart_putchar(mode);
-  build_packet(data);  
-  if(!(PINF & (1 << 0)))
+  initRadioTX(); //turn on transmit mode
+  _delay_ms(8); //let it sink in
+  uart_putchar(mode); 
+  
+  /* WRITE THIS */
+  build_packet(data); //populate packet with collab packet 
+  /*            */
+  
+  if(!(PINF & (1 << 0))) //dont enter transmission unless switch off
     goto exit; 
-  setRadioTXPayload(data, PACKET_SIZE);
+  setRadioTXPayload(data, PACKET_SIZE); //load packet for entry
   CE_HIGH;
   _delay_us(15);  // pulse CE to start transmition
   CE_LOW;
-  if(!(PINF & (1 << 0)))
+  if(!(PINF & (1 << 0)))  //again don't stupid get this far unless switch is still on
     goto exit; 
+
+  /* here is the magic box */
+  /* under all conditions this should not be executed */
   while(!(getTX_DS())){
     if(!(PINF & (1 << 0)))
       goto exit;
@@ -80,11 +100,12 @@ void transmit(uint8_t * data){
         CE_LOW;
     }
   }
+  /* jump here to exit function */
   exit:
     clearTX_DS();
     _delay_ms(5);
 
-    SPI_masterInit();
+    SPI_masterInit(); //set into receive mode 
     initRadioRX();
     _delay_ms(8);
     uart_putstring("switching to receive");
@@ -103,7 +124,8 @@ int main(void){
   uint8_t *load = (uint8_t *)malloc(PACKET_SIZE*sizeof(uint8_t));
 
   CPU_PRESCALE(0x01);  // run at 8 MHz
-
+/* iniitalization, don't touch */
+/*=====================================*/
   INIT_CSN;
   INIT_CE;
   CSN_HIGH;
@@ -122,13 +144,16 @@ int main(void){
 
   sei();
   _delay_ms(5);
+/* ======================================*/
+  
   while(1){
-    _delay_ms(100);
+    _delay_ms(100); //woah there buddy slow down
+    /* set modes and make decision later */
     if(PINF & (1 << 0)){
       mode = 't';
     } 
     else mode = 'r';
-
+    /* if receiving, receive */
     if (mode == 'r'){
       uart_putchar(mode);   
       uint8_t radioStatus = 0;
@@ -145,6 +170,7 @@ int main(void){
       CE_HIGH; 
       _delay_ms(5);
     }
+    /* else transmit */
     else if(mode == 't'){
       transmit(data);
     }
